71c950a0f29b5aae04b9adf4276e4491cedd1003
APHIXE-DESKTOP\jason <aphixe@gmail.com>
2017-09-29 09:04:55 -0700

    first commit

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+	
+	public AudioClip crack;
+	public Sprite[] hitSprites;
+	public static int breakableCount = 0;
+	public GameObject smoke;
+
+	private int timesHit;
+	private LevelManager levelManager;
+	private bool isBreakable;
+
+	// Use this for initialization
+	void Start () {
+		timesHit = 0;
+		isBreakable = (this.tag == "Breakable");
+		if (isBreakable) {
+			breakableCount++;
+			print (breakableCount);
+		}
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnCollisionEnter2D (Collision2D col) {
+		AudioSource.PlayClipAtPoint (crack, transform.position);
+		if (isBreakable) {
+			HandleHits();
+		}
+	}
+
+	void HandleHits() {
+		timesHit++;
+		
+		int maxHits = hitSprites.Length + 1;
+		if (timesHit >= maxHits) {
+			breakableCount--;
+			PuffSmoke();
+			levelManager.BrickDestroyed();
+			print (breakableCount);
+			Destroy (gameObject);
+		} else {
+			LoadSprites();
+		}
+	}
+
+	void PuffSmoke(){
+		GameObject smokePuff = Instantiate(smoke, transform.position, Quaternion.identity) as GameObject;
+		smokePuff.particleSystem.startColor = gameObject.GetComponent<SpriteRenderer>().color;
+	}
+
+	void LoadSprites(){
+		int spriteIndex = timesHit - 1;
+
+		if (hitSprites [spriteIndex] != null) {
+			this.GetComponent<SpriteRenderer> ().sprite = hitSprites [spriteIndex];
+		} else {
+			Debug.LogError ("Brick sprite missing.");
+		}
+	}
+
+	// TODO Remove this method once we can actually win!
+	void SimulateWin () {
+		levelManager.LoadNextLevel();
+	}
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log ("New Level load: " + name);
+		Application.LoadLevel (name);
+		Brick.breakableCount = 0;
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+	
+	public void LoadNextLevel() {
+		Application.LoadLevel(Application.loadedLevel + 1);
+		Brick.breakableCount = 0;
+	}
+
+	public void BrickDestroyed() {
+		if(Brick.breakableCount <= 0){
+			LoadNextLevel();
+		}
+	}
+}
